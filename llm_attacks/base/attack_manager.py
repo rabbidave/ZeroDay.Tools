88d56801416aa2a31e47a4e57b2b791bc59b718c
Nope Nope <d.isaac.pierce@gmail.com>
2023-12-21 13:22:03 -0600

    Fixing Typo

--- llm_attacks/base/attack_manager.py
+++ llm_attacks/base/attack_manager.py
@@ -1488,7 +1488,7 @@ class ModelWorker(object):
             ).to(device).eval()
 
             self.tokenizer = AutoTokenizer  # Assuming the tokenizer is passed correctly
-        elif:
+        else:
             # Use AutoModelForCausalLM for other models
             self.model = AutoModelForCausalLM.from_pretrained(
                 model_path,
